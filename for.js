2b9c6b07e9a9fd7476d3aaa42cd63ef3f8cc72ae
Brandon Yen <Aiumi@users.noreply.github.com>
2016-02-11 00:02:58 -0800

    Update for.js

--- for.js
+++ for.js
@@ -3,10 +3,10 @@ function lookUp(firstName, prop){
 if (contacts[i].firstName === firstName) { // checks if the entered first name matches any of the first names in the directory
   if (contacts[i][prop]) { // checks if the entered property matches any properties of that person
   return contacts[i][prop]; // returns the name and property if found
-  }
 } else {
   return "No such property"; // if the entered property is not found
-} 
+      }
+    } 
   }
   return "No such contact"; // if the entered first name is not found
 }
